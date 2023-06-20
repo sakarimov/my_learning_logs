db9b04c3ec5922fa44f2ce827816a02ba09e9e60
sulthankarimov <sulthankarimov@gmail.com>
2023-06-20 09:34:46 +0700

    test preparation done

--- notes-app-back-end-plugin/src/tokenize/TokenManager.js
+++ notes-app-back-end-plugin/src/tokenize/TokenManager.js
@@ -5,7 +5,7 @@ const TokenManager = {
 	generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
 	generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
 	verifyRefreshToken: (refreshToken) => {
-		try{
+		try {
 			const artifacts = Jwt.token.decode(refreshToken);
 			Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
 			const { payload } = artifacts.decoded;
